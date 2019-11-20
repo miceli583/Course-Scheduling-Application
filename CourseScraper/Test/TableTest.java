e48081fd5373b0c48ee042718864782fdc56af0d
Christopher Chee <cchee1@lsu.edu>
2019-11-20 14:31:00 -0600

    Created class for updating single semesters

--- CourseScraper/Test/TableTest.java
+++ CourseScraper/Test/TableTest.java
@@ -27,32 +27,74 @@ public class TableTest {
      */
     public static void main(String[] args) throws IOException {
         
-        File dirpath = new File("C:\\Users\\CChee1\\Desktop\\CSC 3380\\TigerPlanner\\Test\\TableTest");
+        String semester = args[0];
+        
+        for (int i = 1; i < args.length; i++)
+        {
+            semester += " " + args[i];
+        }
+
+        System.out.println(semester);
+
+        File dirpath = new File("TableTest\\" + semester);
 
         if (!dirpath.exists())
         {
             dirpath.mkdirs();
         }
 
-        PrintWriter writer = new PrintWriter("TableTest\\COMPUTER SCIENCE.txt", "UTF-8");
-        String url = "http://appl101.lsu.edu/booklet2.nsf/f5e6e50d1d1d05c4862584410071cd2e?CreateDocument";
+        ArrayList<String> departments = new ArrayList<String>();
+        ArrayList<String> errors = new ArrayList<String>();
+
+        String url = "http://appl101.lsu.edu/booklet2.nsf/mainframeset";
+        Document main = Jsoup.connect(url).get();
+        Element frame = main.select("frame").get(0);
+        Document selectFrame = Jsoup.connect(frame.absUrl("src")).get();
+        Elements options = selectFrame.getElementsByAttributeValue("name", "Department").get(0).children();
+
+        for (Element option : options)
+        {
+            departments.add(option.text());
+        }
+
+        url = "http://appl101.lsu.edu/booklet2.nsf/f5e6e50d1d1d05c4862584410071cd2e?CreateDocument";
         
         CloseableHttpClient client = HttpClientBuilder.create().build();
         HttpPost post = new HttpPost("http://appl101.lsu.edu/booklet2.nsf/f5e6e50d1d1d05c4862584410071cd2e?CreateDocument");
 
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("%%Surrogate_SemesterDesc", "1"));
-        params.add(new BasicNameValuePair("SemesterDesc", "Spring 2020"));
+        params.add(new BasicNameValuePair("SemesterDesc", semester));
         params.add(new BasicNameValuePair("%%Surrogate_Department", "1"));
-        params.add(new BasicNameValuePair("Department", "CURRICULUM & INSTRUCTION"));
-        post.setEntity(new UrlEncodedFormEntity(params));
+        params.add(new BasicNameValuePair("Department", ""));
 
-        HttpResponse response = client.execute(post);
-        String coursesURL = response.getLastHeader("Location").getValue();
-        
-        try
+        for (String department :departments)
         {
-            Document doc = Jsoup.connect(coursesURL).get();
+            System.out.println(department);
+
+            params.set(3, new BasicNameValuePair("Department", department));
+            post.setEntity(new UrlEncodedFormEntity(params));
+
+            HttpResponse response = client.execute(post);
+            String coursesURL = response.getLastHeader("Location").getValue();
+
+            if (coursesURL.compareTo("https://appl101.lsu.edu/booklet2.nsf/NoCourseDept?readform") == 0)
+            {
+                continue;
+            }
+
+            try
+            {
+                Document doc = Jsoup.connect(coursesURL).get();
+            }
+            catch (IOException goddammitJsoup)
+            {
+                String exception = semester + " " + department;
+                errors.add(exception);
+                continue;
+            }
+
+            Document doc = Jsoup.connect(coursesURL).maxBodySize(0).get();
             Elements pretags = doc.select("pre");
             Element pretext = pretags.get(0);
 
@@ -64,16 +106,34 @@ public class TableTest {
             {
                 char c = pre.charAt(i);
 
-                if (c == '/' || c == '\\')
-                {
-                    c = ' ';
-                }
-
                 if (c == '\n')
                 {
                     if (line.length() > 0)
                     {
-                        if (line.charAt(0) == '(' || (Character.isDigit(line.charAt(2))))
+                        int charIndex = 0;
+                        char firstChar = line.charAt(charIndex);
+                        String firstWord = "";
+
+                        while (firstChar == ' ')
+                        {
+                            charIndex++;
+                            firstChar = line.charAt(charIndex);
+                        }
+
+                        while (firstChar != ' ')
+                        {
+                            firstWord += firstChar;
+                            charIndex++;
+
+                            if (charIndex >= line.length())
+                            {
+                                break;
+                            }
+
+                            firstChar = line.charAt(charIndex);
+                        }
+
+                        if (line.charAt(0) == '(' || Character.isDigit(line.charAt(2)) || firstWord.compareTo("LAB") == 0)
                         {
                             lines.add(line);   
                         }
@@ -87,14 +147,30 @@ public class TableTest {
                 }
             }
 
-            for (String s : lines)
+            PrintWriter writer = new PrintWriter(dirpath + "\\" + department + ".txt", "UTF-8");
+
+            for (String l : lines)
             {
-                System.out.println(s);
+                writer.printf("%s\n", l);
             }
+
+            writer.close();
         }
-        catch (IOException e)
+
+        File errlogdir = new File("TableTest\\Logs");
+
+        if (!errlogdir.exists())
         {
-            System.out.println("Uh oh");
+            errlogdir.mkdirs();
         }
+
+        PrintWriter errwriter = new PrintWriter(errlogdir + "\\ErrorLog.txt");
+
+        for (String error : errors)
+        {
+            errwriter.println(error);
+        }
+
+        errwriter.close();
     }
 }
