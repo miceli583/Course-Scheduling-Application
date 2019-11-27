14386be75719bf010ab23919d2f5d7e72b83ce63
Smcfad3 <55609193+Smcfad3@users.noreply.github.com>
2019-11-26 19:58:56 -0600

    Create file.java

---
+++ file.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Course
+{
+        String semester;
+        String dept;
+        String deptAbbrev;
+        String courseNum;
+        int numSections = 0;
+        String courseTitle;
+
+        ArrayList<Section> sections = new ArrayList<Section>();
+
+        void update(){
+                numSections = sections.size();
+        }
+
+        public Course(String s, String d, String a, String n){
+                semester = s;
+                dept = d;
+                deptAbbrev = a;
+                courseNum = n;
+        }
+}
