4e8ad6dde9e7f34a1040ee01c547f9fd85b02c6c
miceli583 <mmicel583@gmail.com>
2019-12-06 14:08:45 -0600

    Final

--- Scheduler/Test1.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Test1{
-	public static void main(String[] args){
-		String input[] = new String[] {"Fall","2019","Computer Science","3200","Computer Science","3380","Computer Science","4444","Economics","2030","Mechanical Engineering","4183","Mechanical Engineering","4201"};
-		Main.main(input);	
-	}
-}
